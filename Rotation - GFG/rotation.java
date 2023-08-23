060ae112d73354c9f9f7e018aa9eb704a3984bb9
Aryan Singh <singh.aryan837@gmail.com>
2023-08-23 22:47:14 +0530

    Added solution - LeetHub

--- Rotation - GFG/rotation.java
+++ Rotation - GFG/rotation.java
@@ -32,9 +32,11 @@ class GFG {
 class Solution {
     int findKRotation(int arr[], int n) {
         // code here
-        for(int i=0;i<n-1;i++){
-            if(arr[i]>arr[i+1]){
-                return i+1;
+        if(arr[0]>arr[n-1]){
+            for(int i=0;i<n-1;i++){
+                if(arr[i]>arr[i+1]){
+                    return i+1;
+                }
             }
         }
         return 0;
