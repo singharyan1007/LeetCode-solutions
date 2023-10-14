1885e422d64a0a6cde2745426a64db05dc762d26
Aryan Singh <singh.aryan837@gmail.com>
2023-10-14 15:34:51 +0530

    Time: 0 ms, Memory: 40.5 MB - LeetHub

---
+++ powx-n/powx-n.java
@@ -0,0 +1,13 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if (n == 0)
+      return 1;
+    if (n < 0)
+      return 1 /(x* myPow(x, -(n+1)));
+    if (n % 2 == 1)
+      return x * myPow(x, n - 1);
+    return myPow(x * x, n / 2);
+        
+        
+    }
+}
